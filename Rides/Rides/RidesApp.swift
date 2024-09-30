3478985493c9bfafb814a8ddb08ade236be8dae6
dineshabe <dineshabe@gmail.com>
2024-09-29 18:44:39 -0600

    Initial Commit

---
+++ Rides/Rides/RidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RidesApp.swift
+//  Rides
+//
+//  Created by Dinesh Thangasamy on 2024-09-29.
+//
+
+import SwiftUI
+
+@main
+struct RidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
